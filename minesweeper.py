5fd84fa1387aec5f4f67424897dfee1b7a4face9
BrianJ06 <105462729+BrianJ06@users.noreply.github.com>
2022-06-15 04:13:02 -0400

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -32,13 +32,14 @@ class Tile(object):
             self.clickColor = '#d7b899'
         elif self.color == '#aad751':
             self.clickColor = '#e5c29f'
-        #hovering tiles
+        #hovering and clicking tiles
         self.drawing.bind("<Enter>", self.on_enter)
         self.drawing.bind("<Leave>", self.on_leave)
         self.drawing.bind("<Button-1>", self.left_click)
         self.drawing.bind("<Button-2>", self.right_click)
         self.drawing.bind("<Button-3>", self.right_click)
 
+        #create 2d list of tiles
         self.x = x
         self.y = y
         coords[y][x] = self
@@ -50,6 +51,7 @@ class Tile(object):
     def on_leave(self, e):
         self.drawing['background']=self.color
 
+    #randomly place mines
     def placeMines(self):
         chance = [0, 0, 0, 0, 0, 1]
         num = random.choice(chance)
@@ -57,11 +59,13 @@ class Tile(object):
             self.isMine = True
             self.clickColor = 'red'
 
+    #left click
     def left_click(self, e):
         global firstClick
         global gameOver
         global numClicked
 
+        #place mines and spread tiles after first click
         if gameOver == False:
             if firstClick == True:
                 near = self.nearTiles()
@@ -74,6 +78,7 @@ class Tile(object):
                 for tile in tiles:
                     tile.nearMines()
 
+                #start timer and mine counter
                 global start
                 start = time.time()
 
@@ -81,9 +86,10 @@ class Tile(object):
                 window.after(1, showTime)
                 firstClick = False
 
+            #game is lost if mine is clicked
             if self.isMine == True and self.markedMine == False:
                 gameOver = True
-                playsound("C:/Users/rando/Downloads/sounds_gameLose.mp3", block=False)
+                playsound("assets/minesweeperassets/sounds_gameLose.mp3", block=False)
                 for tile in tiles:
                     if tile.isMine == True:
                         tile.click()
@@ -95,34 +101,29 @@ class Tile(object):
                 tk.Button(window, text='⭮ Try Again', font=('Fixedsys', 24), command=self.restart, fg='white',
                           bg='#4a752c', bd=0, padx=25, pady=15).grid(row=9, column=6, columnspan=8, rowspan=3)
 
+            #spread mines if regular click
             if gameOver == False:
                 if self.markedMine == False:
                     if self.numMines == 0:
                         self.spread()
                     self.click()
 
-                playsound("C:/Users/rando/Downloads/sounds_click.wav", block=False)
+                #click sound
+                playsound("assets/minesweeperassets/sounds_click.wav", block=False)
 
+                #game is won if all non-mine tiles are clicked
                 total = totalMines()
                 if numClicked == 300-total:
                     gameOver = True
-                    playsound("C:/Users/rando/Downloads/sounds_gameWin.wav", block=False)
+                    playsound("assets/minesweeperassets/sounds_gameWin.wav", block=False)
                     tk.Label(window, text='You win!', font=('Fixedsys', 36, 'bold'), anchor='n', fg='white', bg='#4a752c',
                              pady=20, width=12, height=2).grid(row=4, column=5, rowspan=7, columnspan=10)
 
-                    current = time.time()
-                    timePassed = str(int(current - start))
-
-                    if len(timePassed) == 1:
-                        text='00' + timePassed
-                    elif len(timePassed) == 2:
-                        text='0' + timePassed
-                    elif len(timePassed) == 3:
-                        text=timePassed
+                    text = timeLabel['text']
 
                     tk.Label(window, image=clock, bg='#4a752c').grid(row=7, column=8, columnspan=2, rowspan=2)
                     tk.Label(window, text=text, font=('Fixedsys', 30, 'bold'), fg='white', bg='#4a752c').grid(
-                        row=7, column=9, columnspan=3, rowspan=2)
+                        row=7, column=9, columnspan=4, rowspan=2)
                     tk.Button(window, text='⭮ Try Again', font=('Fixedsys', 24), command=self.restart, fg='white',
                               bg='#4a752c', bd=0, padx=25, pady=15).grid(row=9, column=6, columnspan=8, rowspan=3)
 
@@ -142,6 +143,7 @@ class Tile(object):
             self.isClicked = True
             numClicked += 1
 
+    #find nearby tiles
     def nearTiles(self):
         nearTiles = []
 
@@ -155,6 +157,7 @@ class Tile(object):
                             nearTiles.append(tile)
         return nearTiles
 
+    #spread until all edge tiles have nearby mines
     def spread(self):
         nearTiles = self.nearTiles()
         while True:
@@ -173,6 +176,7 @@ class Tile(object):
         for tile in nearTiles:
             tile.click()
 
+    #find number of nearby mines
     def nearMines(self):
         self.numMines = 0
 
@@ -199,6 +203,7 @@ class Tile(object):
                 if self.numMines == 7:
                     self.numColor = '#464544'
 
+    #mark mines with right click
     def right_click(self, e):
         global flag
         global gameOver
@@ -207,15 +212,16 @@ class Tile(object):
                 if self.markedMine == False:
                     self.drawing.configure(image=flag, width=16, height=32)
                     self.markedMine = True
-                    playsound("C:/Users/rando/Downloads/sounds_flag.wav", block=False)
+                    playsound("assets/minesweeperassets/sounds_flag.wav", block=False)
                 elif self.markedMine == True:
                     self.drawing.configure(image='', width=2, height=2)
                     self.markedMine = False
-                    playsound("C:/Users/rando/Downloads/sounds_unflag.wav", block=False)
+                    playsound("assets/minesweeperassets/sounds_unflag.wav", block=False)
 
+    #restart program
     def restart(self):
         window.destroy()
-        os.execl(sys.executable, 'python', "C:/Users/rando/PycharmProjects/games/minesweeper.py", *sys.argv[1:])
+        os.execl(sys.executable, 'python', "minesweeper.py", *sys.argv[1:])
 
 #title
 tk.Label(window, text='Minesweeper', font=('Fixedsys', 29), fg='white', bg='#4a752c', width=44).grid(
@@ -235,7 +241,7 @@ for y in range(15):
         tile.drawing.grid(row=y+2, column=x, ipadx=15, ipady=8)
 
 #flag image for counter and marking mines
-flag = Image.open("C:/Users/rando/Downloads/minesweeperflag.png")
+flag = Image.open("assets/minesweeperassets/minesweeperflag.png")
 flag = flag.convert("RGBA")
 
 pixels = flag.getdata()
@@ -253,6 +259,7 @@ tk.Label(window, image=flag, bg='#4a752c', width=33).grid(row=0, column=0, colum
 minesLabel = tk.Label(window, text="=  ??? ", font=('Fixedsys', 18), fg='white', bg='#4a752c')
 minesLabel.grid(row=0, column=2, columnspan=2)
 
+#count mines
 def totalMines():
     totalMines = 0
     for tile in tiles:
@@ -270,7 +277,7 @@ def showMines():
     window.after(100, showMines)
 
 #clock image for time counter
-clock = Image.open("C:/Users/rando/Downloads/minesweeperclock.png")
+clock = Image.open("assets/minesweeperassets/minesweeperclock.png")
 clock = clock.resize((41, 51))
 clock = ImageTk.PhotoImage(clock)
 
@@ -283,36 +290,19 @@ timeLabel.grid(row=0, column=17, columnspan=2)
 
 #update time counter every second
 def showTime():
-    current = time.time()
-    timePassed = str(int(current-start))
-    if int(timePassed) <= 999:
-        if len(timePassed) == 1:
-            timeLabel.config(text='00'+timePassed+'  ')
-        elif len(timePassed) == 2:
-            timeLabel.config(text='0'+timePassed+'  ')
-        elif len(timePassed) == 3:
-            timeLabel.config(text=timePassed+'  ')
+    if gameOver == False:
+        current = time.time()
+        timePassed = str(int(current-start))
+        if int(timePassed) <= 999:
+            if len(timePassed) == 1:
+                timeLabel.config(text='00'+timePassed+'  ')
+            elif len(timePassed) == 2:
+                timeLabel.config(text='0'+timePassed+'  ')
+            elif len(timePassed) == 3:
+                timeLabel.config(text=timePassed+'  ')
 
-        global gameOver
-        if gameOver == False:
-            window.after(1000, showTime)
-        elif gameOver == True:
-            if int(timePassed) < 999:
-                timePassed = int(timePassed)
-                timePassed -= 1
-                timePassed = str(timePassed)
-                if len(timePassed) == 1:
-                    timeLabel.config(text='00' + timePassed + '  ')
-                elif len(timePassed) == 2:
-                    timeLabel.config(text='0' + timePassed + '  ')
-                elif len(timePassed) == 3:
-                    timeLabel.config(text=timePassed + '  ')
+        window.after(1000, showTime)
 
-
-def loopSong():
-    playsound("C:/Users/rando/Downloads/sounds_music.mp3", block=False)
-    window.after(169000, loopSong)
-loopSong()
 
 # show window
 window.mainloop()
